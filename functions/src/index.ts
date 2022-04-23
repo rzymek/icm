ff54904649a86045346af82c0dd48ceaf2cd677e
Krzysztof Rzymkowski <rzymek@gmail.com>
2022-04-24 00:51:25 +0200

    fetch links

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,10 @@ import * as functions from "firebase-functions";
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+export const links = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", { structuredData: true });
+    response.send(JSON.stringify({
+        icm48: "http://new.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=409&col=248&lang=pl",
+        icm84: "https://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=395&col=262&lang=pl",
+    }));
 });
