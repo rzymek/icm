c3bbb521ea845db611c7c10aaacb818e24a5df50
Krzysztof Rzymkowski <rzymek@gmail.com>
2022-04-24 02:08:15 +0200

    blue meteo

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,4 @@
+
 import * as functions from "firebase-functions";
 import axios from "axios";
 
@@ -6,23 +7,55 @@ interface LatLng {
     lng: string;
 }
 
+function errorLink(e: Error) {
+    functions.logger.error(e, { structuredData: true });
+    return undefined;
+}
 async function icm(prefix: '' | '/um', { lat, lng }: LatLng) {
-    const url = `http://new.meteo.pl${prefix}/php/mgram_search.php?NALL=${lat}&EALL=${lng}`;
-    console.log(url);
-    const resp = await axios.get(url, { maxRedirects: 0, validateStatus: null });
-    const [row, col] = resp.headers.location.match(/(row|col)=([0-9]+)/g)?.map(match => match.split(/=/)[1]) ?? [0, 0]
-    return `http://new.meteo.pl${prefix}/metco/mgram_pict.php?ntype=0u&row=${row}&col=${col}&lang=pl`
+    try {
+        const url = `http://new.meteo.pl${prefix}/php/mgram_search.php?NALL=${lat}&EALL=${lng}`;
+        console.log(url);
+        const resp = await axios.get(url, { maxRedirects: 0, validateStatus: null });
+        const [row, col] = resp.headers.location.match(/(row|col)=([0-9]+)/g)?.map(match => match.split(/=/)[1]) ?? []
+        if(row === undefined || col == undefined) {
+            throw new Error(`pattern not found in location header: ${resp.headers.location}`);
+        }
+        return `http://new.meteo.pl${prefix}/metco/mgram_pict.php?ntype=0u&row=${row}&col=${col}&lang=pl`
+    } catch (e: any) {
+        return errorLink(e);
+    }
+}
+
+async function blueMeteo(type: 'tydzie%C5%84' | '14-dniowa', c: LatLng) {
+    try {
+        const url = `https://www.meteoblue.com/pl/pogoda/${type}/${c.lat}N${c.lng}E`
+        console.log(url);
+        const resp = await axios.get(url);
+        const [link] = resp.data?.match('//my.meteoblue.com/visimage/[^"]*');
+        return `https://${link}`;
+    } catch (e: any) {
+        return errorLink(e);
+    }
 }
 
 async function run(coord: LatLng, response: functions.Response<any>) {
-    response.send(JSON.stringify({
-        icm48: await icm('/um', coord),
-        icm84: await icm('', coord),
-    }));
+    const config = {
+        icm48: icm('/um', coord),
+        icm84: icm('', coord),
+        blue7: blueMeteo('tydzie%C5%84', coord),
+        blue14: blueMeteo('14-dniowa', coord),
+    };
+    const results = Object.fromEntries(
+        (await Promise.all(
+            Object.entries(config)
+                .map(([name, promise]) => promise.then(result => ([name, result])))
+        )).filter(([, result]) => result)
+    );
+    response.send(JSON.stringify(results));
 }
 
 export const links = functions.https.onRequest((request, response) => {
     const { lat, lng } = request.query;
-    functions.logger.info("Hello logs!", { structuredData: true });
+
     run({ lat: lat as string, lng: lng as string }, response);
 });
